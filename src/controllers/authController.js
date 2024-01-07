fb88a7a7ed9dc85d0597bf2fd161340c21822bd1
Yogesh01000100 <yogeshone678@gmail.com>
2024-01-07 13:02:57 +0530

    feat(auth): implement JWT token authentication
    - create login route
    
    Signed-off-by: Yogesh01000100 <yogeshone678@gmail.com>

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { userExists } from "../services/userServices.js"
+import { userExists } from "../services/userServices.js";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
@@ -18,8 +19,8 @@ export async function signup(req, res) {
           email,
         },
       });
-
-      return res.status(201).json({ msg: `User created with id: ${user.id}` });
+      const token = jwt.sign({ username: name }, process.env.SECRET);
+      return res.status(201).json({ msg: `User created with id: ${user.id} Token: ${token}` });
     } else {
       return res.status(409).json({ msg: "User Already Exists!" });
     }
@@ -31,8 +32,26 @@ export async function signup(req, res) {
 
 export async function login(req, res) {
   try {
-    // Add login functionality here
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return res.status(401).json({ error: "Authorization header missing" });
+    }
+
+    const [bearer, token] = authHeader.split(" ");
+
+    if (bearer !== "Bearer" || !token) {
+      return res.status(401).json({ error: "Invalid Authorization header format" });
+    }
+
+    const verified_Token = jwt.verify(token, process.env.SECRET);
+    if (verified_Token) {
+      const name = jwt.decode(token);
+      return res.status(201).json({ msg: `userName: ${name.username}` });
+    } else {
+      return res.status(401).json({ error: "Invalid Token" });
+    }
   } catch (error) {
-    // Handle login error here
+    res.status(500).json({ msg: `Error ${error}` });
   }
 }
