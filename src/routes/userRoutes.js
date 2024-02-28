41c26fc5b020e9fb01a8cfbda22b5c7936b7b7a7
D.Yogesh <yogeshone678@gmail.com>
2024-02-28 21:43:02 +0530

    feat(routes): separate authroutes
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,9 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
-import { signup, login } from "../controllers/authController.js";
 import { getMe, getReview, createReview } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
 router.get("/getme", verifyToken, getMe);
 router.get("/getreview/:movieId", verifyToken, getReview);
 router.post("/createreview", verifyToken, createReview);
