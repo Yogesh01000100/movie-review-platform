2fb1067e890c276028a892ee4b8671fd7de2dfb6
D.Yogesh <yogeshone678@gmail.com>
2024-02-28 18:10:13 +0530

    chore: update the files
    Signed-off-by: D.Yogesh <yogeshone678@gmail.com>

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
 import { signup, login } from "../controllers/authController.js";
-import { getMe, getReview, createReview } from "../controllers/userController.js";
+import { getMe } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.get("/getme", verifyToken, getMe);
-router.get("/getreview/:movieId", verifyToken, getReview);
-router.post("/createreview", verifyToken, createReview);
 
 export default router;
